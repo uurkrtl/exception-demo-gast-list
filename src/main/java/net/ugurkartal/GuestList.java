12fc1f05e578c553741ad4cdca98294c8bb0a078
Ugur Kartal <kartalugur@gmail.com>
2024-02-09 22:05:42 +0100

    add Test and addGuest method

--- src/main/java/net/ugurkartal/GuestList.java
+++ src/main/java/net/ugurkartal/GuestList.java
@@ -3,22 +3,20 @@ package net.ugurkartal;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GuestList {
     List<Guest> guests = new ArrayList<>();
-    Path filePath = Path.of("guests.csv");
+    private final Path FILE_PATH = Path.of("guests.csv");
 
     public List<Guest> getGuests() {
         try {
-            return Files.lines(filePath)
+            return Files.lines(FILE_PATH)
                     .filter(line -> !line.isBlank())
-                    .map(line -> {
-                        //String[] parts = line.split(",");
-                        return new Guest(line);
-                    })
+                    .map(line -> {return new Guest(line);})
                     .collect(Collectors.toList());
         }catch (IOException e) {
             e.printStackTrace();
@@ -28,11 +26,22 @@ public class GuestList {
 
     public void setGuests(List<Guest> guests) {
         try {
-            Files.write(filePath, guests.stream()
+            Files.write(FILE_PATH, guests.stream()
                     .map(guest -> guest.getName())
                     .collect(Collectors.toList()));
         }catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Guest> getGuests(Path of) throws IOException {
+            return Files.lines(of)
+                    .filter(line -> !line.isBlank())
+                    .map(line -> {return new Guest(line);})
+                    .collect(Collectors.toList());
+    }
+
+    public void addGuest(Guest guest) throws IOException {
+        Files.write(FILE_PATH, List.of(guest.getName()), StandardOpenOption.APPEND);
     }
 }
--- src/test/java/net/ugurkartal/GuestListTest.java
+++ src/test/java/net/ugurkartal/GuestListTest.java
@@ -3,6 +3,8 @@ package net.ugurkartal;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +51,31 @@ class GuestListTest {
 
         List<Guest> expected = List.of(new Guest("Stephan"), new Guest("Max"));
         guestList.setGuests(expected);
+
+        List<Guest> actual = guestList.getGuests();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReadFromFileSystem_whenFileNotExist() {
+        GuestList guestList = new GuestList();
+
+        List<Guest> expected = List.of(new Guest("Stephan"), new Guest("Max"));
+        guestList.setGuests(expected);
+
+        assertThrows(NoSuchFileException.class, () -> guestList.getGuests(Path.of("notExist.csv")));
+    }
+
+    @Test
+    void testAddGuest() throws IOException {
+        GuestList guestList = new GuestList();
+        guestList.setGuests(List.of(new Guest("Stephan"), new Guest("Max")));
+
+        List<Guest> expected = List.of(new Guest("Stephan"), new Guest("Max"), new Guest("Ugur"));
+
+        Guest newGuest = new Guest("Ugur");
+        guestList.addGuest(newGuest);
 
         List<Guest> actual = guestList.getGuests();
 
